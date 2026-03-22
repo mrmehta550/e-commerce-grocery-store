ff62709924439411e913380fd0bc1995655f9473
mrmehta550 <vishalmehta9117@gmail.com>
2026-03-23 05:09:23 +0530

    Fix some issue after deployment

--- freshshop/settings.py
+++ freshshop/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = 'django-insecure-9-+j6(@h+g=icmgj9=cl#%1djuuqsyqfn07c)l6ae0h&ytn$s$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mrmehta5500.pythonanywhere.com',
+                 '127.0.0.1',
+                 'localhost'
+                 ]
 
 
 # Application definition
@@ -131,7 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- freshshop/wsgi.py
+++ freshshop/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for freshshop project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
+path = '/home/mrmehta5500/freshshop'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'freshshop.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freshshop.settings')
-
 application = get_wsgi_application()
